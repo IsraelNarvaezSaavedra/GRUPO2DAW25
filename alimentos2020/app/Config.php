79e0ef24ea37fad1753929488577195e546aacee
Israel Narvaez Saavedra <narvaez.saavedra.israel@iescamas.es>
2025-12-04 12:23:15 +0100

    Lo he tenido que hacer yo porque alicia se fue al medico, Config terminado

---
+++ alimentos2020/app/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+ class Config
+ {
+     static public $mvc_bd_hostname = "mysql-clarafp8.alwaysdata.net";
+     static public $mvc_bd_nombre   = "clarafp8_alimentos22025";
+     static public $mvc_bd_usuario  = "clarafp8";
+     static public $mvc_bd_clave    = "HostingClara2025";
+     static public $mvc_vis_css     = "estilo.css";
+ }
+?>
